aa64c21b4fa34d3df23e15ffb37877019b971715
Satyam <satyamgarde30@gmail.com>
2022-05-21 23:51:57 +0530

    Tut3

---
+++ Tut4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a = 4;
+    float b = 7;
+    cout << "This is tutorial 4" << endl;
+    cout << "Value of a " << a<<endl << "Value of b" << b << endl;
+    return 0;
+}
