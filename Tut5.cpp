87fb4aabe1a64057255203c9f8e781ddf5703d67
Satyam <satyamgarde30@gmail.com>
2022-05-22 21:57:34 +0530

    Tut5

---
+++ Tut5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int num1;
+int num2;
+int main()
+{
+    cout<<"enter first number"<<endl;
+    cin>>num1;
+    cout<<"enter second number"<<endl;
+    cin>>num2;
+    cout<<"sum of two numbers is"<<num1 + num2<<endl;
+
+    return 0;
+}
