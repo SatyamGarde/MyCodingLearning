5a73077be1a07b710f809c2e3a893294e85288fd
Satyam <satyamgarde30@gmail.com>
2022-05-28 02:48:08 +0530

    Tut10

---
+++ Tut10.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*Loops in c++
+    There are three types in c++
+    1. for Loop
+    2. While loop
+    3.do-While loop
+    */
+
+    /*1.For Loop*/
+    //   Syntax for for loop
+    /*for(initialization,condition,updation)
+    {
+        loop body
+    }*/
+    // for (int i= 0; i <= 40; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // Example of infinite loop
+
+    // for (int i = 0; 34 < 40; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // While loop in c++
+    // while ( condition)
+    // {
+    //     c++ Statements
+    // }
+    // int i=1;
+    // while (i<=40)
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+
+    // Example for Infinite While loop
+    //     int i=1;
+    //     while (true)
+    //     {
+    //         cout<<i<<endl;
+    //         i++;
+    //     }
+
+    return 0;
+}
