87c743bcd0824254ce231cc96941bb7949e8b78d
Satyam <satyamgarde30@gmail.com>
2022-06-28 21:21:55 +0530

    Tut52

---
+++ Tut52.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class ShopItem
+{
+    int id;
+    float price;
+    public:
+        void setData(int a, float b){
+            id = a;
+            price = b;
+        }
+        void getData(void){
+            cout<<"Code of this item is "<< id<<endl;
+            cout<<"Price of this item is "<<price<<endl;
+        }
+};
