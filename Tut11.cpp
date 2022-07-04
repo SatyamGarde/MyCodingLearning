9e6b826b50737cf9f5fc4c6f5a2d5a65114057b2
Satyam <satyamgarde30@gmail.com>
2022-07-04 23:52:08 +0530

    Tut11 changes

--- Tut11.cpp
+++ Tut11.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int main(){
-    // for (int i = 0; i < 40; i++)
-    // {
-    //     /* code */
-    //     if(i==2){
-    //         break;
-    //     }
-    //     cout<<i<<endl;
-    // }
     for (int i = 0; i < 40; i++)
     {
         /* code */
         if(i==2){
-            continue;
+            break;
         }
         cout<<i<<endl;
     }
+    // for (int i = 0; i < 40; i++)
+    // {
+    //     /* code */
+    //     if(i==2){
+    //         continue;
+    //     }
+    //     cout<<i<<endl;
+    // }
 
     
     return 0;
