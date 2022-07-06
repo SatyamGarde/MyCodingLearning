60dec52970e3d078c49596d5cfd8c56b55ebe0fa
Satyam <satyamgarde30@gmail.com>
2022-07-06 23:33:40 +0530

    Tut13

--- Tut13.cpp
+++ Tut13.cpp
@@ -36,8 +36,8 @@ int main(){
     int* p = marks;
     cout<<*(p++)<<endl;
     cout<<*(++p)<<endl;
-    // cout<<"The value of *p is "<<*p<<endl;
-    // cout<<"The value of *(p+1) is "<<*(p+1)<<endl;
+    cout<<"The value of *p is "<<*p<<endl;
+    cout<<"The value of *(p+1) is "<<*(p+1)<<endl;
     // cout<<"The value of *(p+2) is "<<*(p+2)<<endl;
     // cout<<"The value of *(p+3) is "<<*(p+3)<<endl; 
     
